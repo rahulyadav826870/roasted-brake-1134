f37d30bb9ed5d5d3db44232e7bb1bc3257339e6b
Addisharma19 <106029673+Addisharma19@users.noreply.github.com>
2022-08-26 22:52:57 +0530

    day3

---
+++ script/index.js
@@ -0,0 +1,29 @@
+type="text/javascript">
+    $('.main-carousel').flickity({
+  // options
+  cellAlign: 'left',
+  wrapAround:true,
+  freeScroll: true
+});
+type="text/javascript">
+    $('.hight').flickity({
+  // options
+  cellAlign: 'left',
+  wrapAround:true,
+  freeScroll: true
+});
+type="text/javascript">
+    $('.last1').flickity({
+  // options
+  cellAlign: 'left',
+  wrapAround:true,
+  freeScroll: true
+});
+type="text/javascript">
+    $('.last').flickity({
+  // options
+  cellAlign: 'left',
+  wrapAround:true,
+  freeScroll: true
+});
+
